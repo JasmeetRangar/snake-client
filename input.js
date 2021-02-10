99e7565bd322adcc8279abea78c97f755ef40394
Jasmeet Rangar <jasmeetsingr@gmail.com>
2021-02-10 20:02:20 +0000

    alter file to handle user input and make snake move

--- input.js
+++ input.js
@@ -1,13 +1,27 @@
+let connection;
 const handleUserInput = (key) => {
   if (key === '\u0003') {
     process.exit();
+  }
+  else if(key === 'w') {
+    connection.write("Move: up");
+  }
+  else if(key === 's') {
+    connection.write("Move: down");
+  }
+  else if(key === 'd') {
+    connection.write("Move: right");
+  }
+  else if(key === 'a') {
+    connection.write("Move: left");
   }
 }
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
